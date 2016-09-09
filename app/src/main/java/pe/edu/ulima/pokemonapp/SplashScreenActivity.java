a583dd4c3ea2db7c1bc4ad4300ab61bd458627f6
Jose Luis Granados <joseluisgd95@gmail.com>
2016-09-09 18:58:22 -0500

    DISP MOVILES

---
+++ app/src/main/java/pe/edu/ulima/pokemonapp/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package pe.edu.ulima.pokemonapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private final int DURACION_SPLASH=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
